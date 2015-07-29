0a37ea2ef13f836d932be9fa59b6d3b164675263
Ricky Tran <rickytran991@gmail.com>
2015-07-28 22:37:14 -0700

    Completed and usable.

---
+++ DicomDecompresser/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DicomDecompresser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectInputDirectory = new System.Windows.Forms.Button();
+            this.txtInputDirectory = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSelectOutputDirectory = new System.Windows.Forms.Button();
+            this.txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtOutputLog = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectInputDirectory);
+            this.groupBox1.Controls.Add(this.txtInputDirectory);
+            this.groupBox1.Location = new System.Drawing.Point(28, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Directory";
+            // 
+            // btnSelectInputDirectory
+            // 
+            this.btnSelectInputDirectory.Location = new System.Drawing.Point(153, 17);
+            this.btnSelectInputDirectory.Name = "btnSelectInputDirectory";
+            this.btnSelectInputDirectory.Size = new System.Drawing.Size(41, 23);
+            this.btnSelectInputDirectory.TabIndex = 1;
+            this.btnSelectInputDirectory.Text = "...";
+            this.btnSelectInputDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectInputDirectory.Click += new System.EventHandler(this.btnSelectInputDirectory_Click);
+            // 
+            // txtInputDirectory
+            // 
+            this.txtInputDirectory.Location = new System.Drawing.Point(6, 19);
+            this.txtInputDirectory.Name = "txtInputDirectory";
+            this.txtInputDirectory.ReadOnly = true;
+            this.txtInputDirectory.Size = new System.Drawing.Size(141, 20);
+            this.txtInputDirectory.TabIndex = 0;
+            this.txtInputDirectory.TextChanged += new System.EventHandler(this.txtInputDirectory_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSelectOutputDirectory);
+            this.groupBox2.Controls.Add(this.txtOutputDirectory);
+            this.groupBox2.Location = new System.Drawing.Point(28, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 56);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output Directory";
+            // 
+            // btnSelectOutputDirectory
+            // 
+            this.btnSelectOutputDirectory.Location = new System.Drawing.Point(153, 17);
+            this.btnSelectOutputDirectory.Name = "btnSelectOutputDirectory";
+            this.btnSelectOutputDirectory.Size = new System.Drawing.Size(41, 23);
+            this.btnSelectOutputDirectory.TabIndex = 1;
+            this.btnSelectOutputDirectory.Text = "...";
+            this.btnSelectOutputDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectOutputDirectory.Click += new System.EventHandler(this.btnSelectOutputDirectory_Click);
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.Location = new System.Drawing.Point(6, 19);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.ReadOnly = true;
+            this.txtOutputDirectory.Size = new System.Drawing.Size(141, 20);
+            this.txtOutputDirectory.TabIndex = 0;
+            this.txtOutputDirectory.TextChanged += new System.EventHandler(this.txtOutputDirectory_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(34, 180);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtOutputLog
+            // 
+            this.txtOutputLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputLog.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtOutputLog.Location = new System.Drawing.Point(271, 34);
+            this.txtOutputLog.Multiline = true;
+            this.txtOutputLog.Name = "txtOutputLog";
+            this.txtOutputLog.ReadOnly = true;
+            this.txtOutputLog.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtOutputLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutputLog.Size = new System.Drawing.Size(328, 131);
+            this.txtOutputLog.TabIndex = 4;
+            this.txtOutputLog.TextChanged += new System.EventHandler(this.txtOutputLog_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 217);
+            this.Controls.Add(this.txtOutputLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dicom Decompresser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSelectInputDirectory;
+        private System.Windows.Forms.TextBox txtInputDirectory;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSelectOutputDirectory;
+        private System.Windows.Forms.TextBox txtOutputDirectory;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtOutputLog;
+    }
+}
+
---
+++ DicomDecompresser/Form1.cs
@@ -0,0 +1,103 @@
+﻿/*
+ *  Ricky Region Tran 
+ *  I don't care, you do whatever you want with this program.
+ *  But remember to give thanks to the people at OFFIS
+ *  
+ *     http://dicom.offis.de/dcmtk.php.en
+ *     
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DicomDecompresser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectInputDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowDialog();
+            txtInputDirectory.Text = fb.SelectedPath;
+        }
+
+        private void txtInputDirectory_TextChanged(object sender, EventArgs e)
+        {
+            if (txtInputDirectory.TextLength > 0)
+            {
+                txtOutputLog.Text += "Set input directory to: " + txtInputDirectory.Text + Environment.NewLine;
+                if (txtOutputDirectory.TextLength > 0)
+                    btnStart.Enabled = true;
+                else
+                    btnStart.Enabled = false;
+            }
+            else
+                btnStart.Enabled = false;
+        }
+
+        private void btnSelectOutputDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowDialog();
+            txtOutputDirectory.Text = fb.SelectedPath;
+        }
+
+        private void txtOutputDirectory_TextChanged(object sender, EventArgs e)
+        {
+            if (txtOutputDirectory.TextLength > 0)
+            {
+                txtOutputLog.Text += "Set output directory to: " + txtOutputDirectory.Text + Environment.NewLine;
+                if (txtInputDirectory.TextLength > 0)
+                    btnStart.Enabled = true;
+                else
+                    btnStart.Enabled = false;
+            }
+            else
+                btnStart.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> pathList = Directory.EnumerateFiles(txtInputDirectory.Text);
+            Process p = new Process();
+            p.StartInfo.FileName = "dcmdjpeg.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            foreach(string path in pathList)
+            {
+                string outputFullPathFilename = txtOutputDirectory.Text + '\\' + path.Substring(path.LastIndexOf('\\'));
+                
+                p.StartInfo.Arguments = String.Format("-v \"{0}\" \"{1}\"", path, outputFullPathFilename);
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                txtOutputLog.Text += output + Environment.NewLine;
+                p.WaitForExit();
+            }
+                         
+        }
+
+        private void txtOutputLog_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
